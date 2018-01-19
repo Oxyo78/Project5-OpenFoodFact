ecec7d2cc9e382700955a6dcaa9c8f053386476b
Yohan V <vienne.yohan@gmail.com>
2018-01-20 00:08:50 +0100

    MAJ Class SqlRequest

--- database.py
+++ database.py
@@ -1,61 +1,70 @@
-''' Build the Mysql database
-    V.0.1
-    Author : Yohan Vienne
-'''
 # ! /usr/bin/env python3
 # coding: utf-8
 
+''' Database connector
+    Author : Yohan Vienne  V 0.1
+'''
+
 import pymysql.cursors
 
-def new_dtb():
-    ''' Function to create the database'''
+class SqlRequest:
+    '''Execute some request to the SQL datbase.
+       Create the class with database name, host address of the server,
+       user name and password access'''
 
-    #sql = "CREATE SCHEMA IF NOT EXISTS `DBOpenFoodFacts` DEFAULT CHARACTER SET utf8"
+    def __init__(self, db_name, host_address, user_name, password):
+        '''Class initialisator'''
+        self.db_name = db_name
+        self.host_address = host_address
+        self.user_name = user_name
+        self.password = password
+        self.sql_message = ''
 
-    # Connect to the database
-    db = pymysql.connect(host='localhost', user='root', password='', charset='utf8')
+    def sql_execute(self, execute_line):
+        ''' Function to execute a request to the database'''
+
+        try:
+            # Connect to the database
+            self.db_name = pymysql.connect(host=self.host_address,
+                                           user=self.user_name, password=self.password,
+                                           charset='utf8')
+            cursor = self.db_name.cursor()
+            cursor.execute(execute_line)
+            self.db_name.commit()
+
+        finally:
+            self.db_name.close()
+            print("Commande executer")
 
 
-    # Open database connection
-    #db = PyMySQL.connect("localhost", "testuser", "test123", "TESTDB")
+    def sql_get_answer(self, request_db):
+        '''Ask to the database, this function return the answer'''
 
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
+        try:
+            # Connect to the database
+            self.db_name = pymysql.connect(host=self.host_address,
+                                           user=self.user_name, password=self.password,
+                                           charset='utf8')
+            cursor = self.db_name.cursor()
+            cursor.execute(request_db)
+            self.sql_message = cursor.fetchone()
+            return self.sql_message
 
-    # Drop table if it already exist using execute() method.
-    cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+        finally:
+            self.db_name.close()
 
-    # Create table as per requirement
-    """sql = CREATE TABLE EMPLOYEE (
-    FIRST_NAME  CHAR(20) NOT NULL,
-    LAST_NAME  CHAR(20),
-    AGE INT,  
-    SEX CHAR(1),
-    INCOME FLOAT )"""
-
-    cursor.execute(sql)
-
-    # disconnect from server
-    db.close()
-
-    """ try:
-        with connection.cursor() as cursor:
-            # Create the database
-            for line in sql:
-        cursor.execute(sql)
-        connection.commit()
-
-        with connection.cursor() as cursor:
-            # Read a single record
-            sql = "SELECT * FROM dbopenfoodfacts"
-            cursor.execute(sql)
-            result = cursor.fetchone()
-            print(result)
-    finally:
-        connection.close()
-        #print("Base de donnees creer")"""
 
 if __name__ == '__main__':
     import os
-    new_dtb()
-    os.system("PAUSE")
+    sql = '''CREATE SCHEMA IF NOT EXISTS `DBOpenFoodFacts` DEFAULT CHARACTER SET utf8;
+             USE `DBOpenFoodFacts`;
+             CREATE TABLE IF NOT EXISTS `DBOpenFoodFacts`.`BD_OFF` (`OFF_id` INT NOT NULL AUTO_INCREMENT,
+                                                                   `OFF_name` VARCHAR(45) NOT NULL,
+                                                                   `OFF_url` VARCHAR(150) NOT NULL,
+                                                                   `OFF_product` INT NOT NULL,
+                                                                   PRIMARY KEY(`OFF_id`, `OFF_product`),
+                                                                   UNIQUE INDEX `OFF_product_UNIQUE` (`OFF_product` ASC));
+          ENGINE = InnoDB'''
+    newdb = SqlRequest('', 'localhost', 'root', '')
+    newdb.sql_execute(sql)
+    os.system('PAUSE')
