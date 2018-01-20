991b089de4ea5ed8972b42f8789471157549e848
Yohan V <vienne.yohan@gmail.com>
2018-01-20 21:24:11 +0100

    Add delete_db

--- database.py
+++ database.py
@@ -7,6 +7,19 @@
 
 import pymysql.cursors
 
+# Database SQL 
+db_create = ['CREATE SCHEMA IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8;',
+             'USE `test`;',
+             '''CREATE TABLE IF NOT EXISTS `test`.`BD_OFF` (
+                 `OFF_id` INT NOT NULL AUTO_INCREMENT,
+                 `OFF_name` VARCHAR(45) NOT NULL,
+                 `OFF_url` VARCHAR(150) NOT NULL,
+                 `OFF_product` INT NOT NULL,
+                 PRIMARY KEY(`OFF_id`, `OFF_product`),
+                UNIQUE INDEX `OFF_product_UNIQUE` (`OFF_product` ASC))
+             ENGINE = InnoDB;''']
+
+
 class SqlRequest:
     '''Execute some request to the SQL datbase.
        Create the class with database name, host address of the server,
@@ -20,51 +33,60 @@ class SqlRequest:
         self.password = password
         self.sql_message = ''
 
-    def sql_execute(self, execute_line):
-        ''' Function to execute a request to the database'''
+    def create_db(self):
+        ''' Function to create the database'''
 
         try:
-            # Connect to the database
             self.db_name = pymysql.connect(host=self.host_address,
                                            user=self.user_name, password=self.password,
-                                           charset='utf8')
+                                           database=self.db_name, charset='utf8')
             cursor = self.db_name.cursor()
-            cursor.execute(execute_line)
-            self.db_name.commit()
+            for line in db_create:
+                cursor.execute(line)
+                self.db_name.commit()
 
         finally:
             self.db_name.close()
-            print("Commande executer")
+            print("Base de donnees creer")
 
+    def delete_db(self):
+        '''Delete the database'''
 
-    def sql_get_answer(self, request_db):
-        '''Ask to the database, this function return the answer'''
+        try:
+            self.db_name = pymysql.connect(host=self.host_address,
+                                           user=self.user_name, password=self.password,
+                                           database=self.db_name, charset='utf8')
+            cursor = self.db_name.cursor()
+            cursor.execute("DROP DATABASE test;")
+            self.db_name.commit()
+        
+        finally:
+            self.db_name.close()
+            print("Base de donnees supprimer")
+
+    def sql_request(self, request_db):
+        '''Request to the database, this function return the answer if not null
+           Add the SQL command in parameter when you call the function'''
 
         try:
             # Connect to the database
             self.db_name = pymysql.connect(host=self.host_address,
                                            user=self.user_name, password=self.password,
-                                           charset='utf8')
+                                           database=self.db_name, charset='utf8')
             cursor = self.db_name.cursor()
             cursor.execute(request_db)
+            self.db_name.commit()
             self.sql_message = cursor.fetchone()
             return self.sql_message
 
         finally:
             self.db_name.close()
+            print('Requete SQL exécuter')
 
 
-if __name__ == '__main__':
-    import os
-    sql = '''CREATE SCHEMA IF NOT EXISTS `DBOpenFoodFacts` DEFAULT CHARACTER SET utf8;
-             USE `DBOpenFoodFacts`;
-             CREATE TABLE IF NOT EXISTS `DBOpenFoodFacts`.`BD_OFF` (`OFF_id` INT NOT NULL AUTO_INCREMENT,
-                                                                   `OFF_name` VARCHAR(45) NOT NULL,
-                                                                   `OFF_url` VARCHAR(150) NOT NULL,
-                                                                   `OFF_product` INT NOT NULL,
-                                                                   PRIMARY KEY(`OFF_id`, `OFF_product`),
-                                                                   UNIQUE INDEX `OFF_product_UNIQUE` (`OFF_product` ASC));
-          ENGINE = InnoDB'''
+if __name__ == '__main__':  
+    db_insert = "INSERT INTO bd_off VALUES (NULL, 'Yohan2', 'url.com', '466');"
+
     newdb = SqlRequest('', 'localhost', 'root', '')
-    newdb.sql_execute(sql)
-    os.system('PAUSE')
+    newdb.delete_db()
+    #print(resultat)
