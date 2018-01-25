85524a18144f00e1779e6d12e63d37665b2a98c4
Yohan V <vienne.yohan@gmail.com>
2018-01-25 03:41:26 +0100

    Import json data to the database

--- database.py
+++ database.py
@@ -4,6 +4,8 @@
 ''' Database connector
     Author : Yohan Vienne  V 0.1
 '''
+import urllib.request
+import json
 
 import pymysql.cursors
 
@@ -21,7 +23,7 @@ class SqlRequest:
         self.sql_message = ''
 
     def create_db(self):
-        ''' Function to create the database'''
+        ''' Function to create and download the database'''
 
         try:
             self.db_name = pymysql.connect(host=self.host_address,
@@ -33,13 +35,38 @@ class SqlRequest:
                     command = line.rstrip(";")
                     cursor.execute(command)
                     self.db_name.commit()
+            print("Base de donnees créer")
+            print("Chargement de la base de données...")
+
+            # Get the french categorie database from OpenFoodFacts and add to the local database
+            categories_url = urllib.request.urlopen('https://fr.openfoodfacts.org/categories.json')
+            data = categories_url.read()
+            json_output = json.loads(data.decode("utf_8"))
+
+            # Change the type to str format, control len size and take of the langage prefixe
+            for categorie in json_output["tags"]:
+                cat_name = categorie['name']
+                if (len(cat_name) >= 76) or (len(categorie['url']) >= 151):
+                    continue
+                if cat_name[2:3] == ":":
+                    continue
+                cat_to_add = (cat_name, categorie['url'])
+
+                # Insert into the categories table
+                cursor.execute("INSERT INTO categories (cat_name, cat_url) VALUES (%s, %s)", cat_to_add)
+                self.db_name.commit()
+
+            # Get the total number of the categories table and print
+            cursor.execute("SELECT COUNT(*) FROM categories")
+            self.db_name.commit()
+            self.sql_message = cursor.fetchone()
+            print("Base de données chargé avec %s produits" % (self.sql_message))
 
         except FileNotFoundError:
-            print("Loading error of the file")
+            print("Erreur: Fichier de création de la base de donnees non trouvé")
 
         finally:
             self.db_name.close()
-            print("Base de donnees creer")
 
     def delete_db(self):
         '''Delete the database'''
@@ -51,10 +78,10 @@ class SqlRequest:
             cursor = self.db_name.cursor()
             cursor.execute("DROP DATABASE IF EXISTS DBOpenFoodFacts;")
             self.db_name.commit()
+            print("Base de données supprimer")
 
         finally:
             self.db_name.close()
-            print("Base de donnees supprimer")
 
     def sql_request(self, request_db):
         '''Request to the database, this function return the answer if not null
@@ -70,15 +97,14 @@ class SqlRequest:
             self.db_name.commit()
             self.sql_message = cursor.fetchone()
             return self.sql_message
+            print("Requete SQL exécuter")
 
         finally:
             self.db_name.close()
-            print('Requete SQL exécuter')
 
 
 if __name__ == '__main__':
-    db_insert = "INSERT INTO bd_off VALUES (NULL, 'Yohan2', 'url.com', '466');"
-
+    # Test
     newdb = SqlRequest('', 'localhost', 'root', '')
-    newdb.delete_db()
+    newdb.create_db()
     #print(resultat)
