8318ba076a126342da692811d1cfcab6916594f8
Yohan V <vienne.yohan@gmail.com>
2018-01-29 04:48:24 +0100

    Add main menu

--- database.py
+++ database.py
@@ -35,7 +35,7 @@ class SqlRequest:
                     command = line.rstrip(";")
                     cursor.execute(command)
                     self.db_name.commit()
-            print("Base de donnees créer")
+            print("Base de données créer")
             print("Chargement de la base de données...")
 
             # Get the french categorie database from OpenFoodFacts and add to the local database
@@ -43,7 +43,7 @@ class SqlRequest:
             data = categories_url.read()
             json_output = json.loads(data.decode("utf_8"))
 
-            # Change the type to str format, control len size and take of the langage prefixe
+            # Change the type to str format, control len size and take of the langage prefixe(Cleaning file)
             for categorie in json_output["tags"]:
                 cat_name = categorie['name']
                 if (len(cat_name) >= 76) or (len(categorie['url']) >= 151):
@@ -53,7 +53,7 @@ class SqlRequest:
                 data = (cat_name, categorie['url'])
 
                 # Insert into the categories table
-                cursor.execute("INSERT INTO categories (cat_name, cat_url) VALUES (%s, %s)", data)
+                cursor.execute("INSERT IGNORE INTO categories (cat_name, cat_url) VALUES (%s, %s)", data)
                 self.db_name.commit()
 
             # Get the total number of the categories table and print
@@ -62,11 +62,8 @@ class SqlRequest:
             self.sql_message = cursor.fetchone()
             print("Base de données chargé avec %s produits" % (self.sql_message))
 
-        except FileNotFoundError:
-            print("Erreur: Fichier de création de la base de donnees non trouvé")
-
-        finally:
-            self.db_name.close()
+        except Exception as e:
+            print("Erreur : %s" % e)
 
     def delete_db(self):
         '''Delete the database'''
@@ -80,10 +77,10 @@ class SqlRequest:
             self.db_name.commit()
             print("Base de données supprimer")
 
-        finally:
-            self.db_name.close()
+        except Exception as e:
+            print("Erreur : %s" % e)
 
-    def sql_request(self, request_db):
+    def request_db(self, request_db):
         '''Request to the database, this function return the answer if not null
            Add the SQL command in parameter when you call the function'''
 
@@ -96,15 +93,39 @@ class SqlRequest:
             cursor.execute(request_db)
             self.db_name.commit()
             self.sql_message = cursor.fetchone()
-            print("Requete SQL exécuter")
             return self.sql_message
 
-        finally:
-            self.db_name.close()
+        except Exception as e:
+            print("Erreur : %s" % e)
 
+    def check_db(self):
+        """ Check if the database exist """
+        state = 0
+        try:
+            # Connect to the database
+            self.db_name = pymysql.connect(host=self.host_address,
+                                           user=self.user_name, password=self.password,
+                                           database=self.db_name, charset='utf8')
+            cursor = self.db_name.cursor()
+            database = ("SHOW DATABASES")
+            cursor.execute(database)
+            for database in cursor:
+                database = database[0]
+                if str(database) == "dbopenfoodfacts":
+                    state = 1
+                    break
+                else:
+                    state = 0
+            return state
+
+        except Exception as e:
+            # Don't show the error if the doesn't exist
+            pass
 
 if __name__ == '__main__':
     # Test
-    newdb = SqlRequest('', 'localhost', 'root', '')
-    newdb.create_db()
-    #print(resultat)
+    newdb = SqlRequest('dbopenfoodfacts', 'localhost', 'root', '')
+    #resultat = newdb.check_db()
+    resultat = newdb.request_db("SELECT COUNT(*) FROM user")
+    resultat = resultat[0]
+    print(resultat)
--- program.py
+++ program.py
@@ -1,19 +1,112 @@
 ''' Project 5 - OpenFoodFacts
     Author : Yohan Vienne
 
-    This application get the select product in the Json API from OpenFoodFacts.com to print the console.
+    This application get the select product in the Json API from OpenFoodFacts.com to show in a window app.
     
     Install the requirements.txt to get the packages for this script.
 
 '''
 import urllib.request
 import json
-import database
+import os
+import sys
+import time
 
-x = urllib.request.urlopen('https://fr.openfoodfacts.org/categories.json')
-print(x.read())
+import database as db
+
+
+def clear_prompt():
+    """ Clear the prompt """
+    if sys.platform == "win32":
+        os.system("cls")
+    elif sys.platform == "Linux":
+        os.system("clear")
+
+def create_user():
+    """ Create a user account """
+    pass
+
+def access_sav():
+    pass
+
+def search_product():
+    pass
+
+def destroy_database():
+    pass
+
+def create_database(db_state):
+    """ Option 5 - Create the database and download the categories list """
+    newdb = db.SqlRequest('', 'localhost', 'root', '')
+    if db_state == 0:
+        print("")
+        newdb.create_db()
+        print("Retour au menu...")
+        time.sleep(3)
+        menu()
+    else:
+        print("\nLa base de données existe déja, retour au menu...")
+        time.sleep(3)
+        menu()
+
+def menu():
+    """ Main screen of application """
+    choice = 0
+
+    # Check if the database exist
+    newdb = db.SqlRequest('', 'localhost', 'root', '')
+    db_state = newdb.check_db()
+    # Clear prompt
+    clear_prompt()
+    print("\nBienvenue sur la base de données OpenFoodFacts FR.")
+    print("")
+    
+    # Get the User table count
+    if db_state == 1:
+        newdb = db.SqlRequest('dbopenfoodfacts', 'localhost', 'root', '')
+        user = newdb.request_db("SELECT COUNT(*) FROM user")
+        user = user[0]
+        if user == 0:
+            print("Veuillez créer un compte utilisateur pour pouvoir sauvegarder un produit")
+        else:
+            print("Il y a %s utilisateur(s) enregistrer dans la base de données." % user)
+    else:
+        print("Il n'y a aucune base de données actuellement")
+
+    print("*********************************************************************\n")
+    print("Que voulez vous faire ?\n")
+    print("1-Créer un compte utilisateur")
+    print("2-Accédez a vos produits sauvegarder")
+    print("3-Rechercher un produit dans la listes des catégories")
+    print("4-Effacer la base de données actuelle")
+    print("5-Créer la base de données locale")
+    while choice == 0:
+        choice = input("Votre choix (Tapez Q pour quitter): ")
+        # Stop the program
+        if choice.upper() == "Q":
+            break
+        # Check if the input is a digit and between 0 and 6
+        elif choice.isdigit() == False or int(choice) >= 6 or int(choice) == 0:
+            print("\nVous devez entrer un nombre entre 1 et 5\n")
+            choice = 0
+        # Run the input choice
+        elif int(choice) == 1:
+            pass
+        elif int(choice) == 2:
+            pass
+        elif int(choice) == 3:
+            pass
+        elif int(choice) == 4:
+            pass
+        elif int(choice) == 5:
+            create_database(db_state)
+            
+
+
+"""x = urllib.request.urlopen('https://fr.openfoodfacts.org/categories.json')
+print(x.read())"""
 
 
 if __name__ == '__main__':
-    db = database.SqlRequest('', 'localhost', 'root', '')
-    db.delete_db()
+    #newdb = db.SqlRequest('dbopenfoodfacts', 'localhost', 'root', '')
+    menu()
